8fe41d0a74b6ace54f5235d0efd13ad193d248ad
alp <alpdirikgil@gmail.com>
2024-04-27 06:19:55 +0200

    initilized create-react-app

---
+++ shorty-fr/src/Button.js
@@ -0,0 +1,11 @@
+import './Button.css'
+
+function Button() {
+    return (
+        <div className="Button-Container">
+            <button className="Button" id="1">test</button>
+        </div>
+    );
+}
+
+export default Button;
